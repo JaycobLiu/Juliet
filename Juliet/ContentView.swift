13b10f59160d49b3875ac674e3e3ef81fdfe9d8a
Jaycob Liu <Football@2013_20>
2026-08-31 16:53:46 +0800

    Initial Commit

---
+++ Juliet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Juliet
+//
+//  Created by Jaycob liu kaiwen on 31/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Juliet/JulietApp.swift
@@ -0,0 +1,17 @@
+//
+//  JulietApp.swift
+//  Juliet
+//
+//  Created by Jaycob liu kaiwen on 31/8/26.
+//
+
+import SwiftUI
+
+@main
+struct JulietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
