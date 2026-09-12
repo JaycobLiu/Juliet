84eef0e2f101f4e4e2258075edd7b1ef2f21fcd0
Jaycob Liu <Football@2013_20>
2026-09-12 15:10:34 +0800

    y

--- Juliet/ContentView.swift
+++ Juliet/ContentView.swift
@@ -6,6 +6,7 @@
 //
 //come to student corner when ur free on tues and thurs
 ill be there*/
+//Mb i did not see this , i gonna work on it first like today(11 sep) or tmr ( 12 sep) you can just make changes on your own i dont mind
 
 import SwiftUI
 
