aed59b669b7a90d5321bfdc890957863c8c553d8
Jaycob Liu <Football@2013_20>
2026-09-12 16:08:26 +0800

    y

--- Juliet/ContentView.swift
+++ Juliet/ContentView.swift
@@ -7,6 +7,7 @@
 //come to student corner when ur free on tues and thurs
 ill be there*/
 //Mb i did not see this , i gonna work on it first like today(11 sep) or tmr ( 12 sep) you can just make changes on your own i dont mind
+//I have an idea to make this app an app that teaches you how to make Jam
 
 import SwiftUI
 
@@ -14,15 +15,21 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
         VStack {
+                
+                Spacer()
             
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("JUlietJam")
+               
+            NavigationLink{
+                DetailView()
+            }label : {
+                Text("CLICK ME ")
+            }
+            
                 
             }
             
-            .navigationTitle(Text("Welcome to JulietJam"))
+            .navigationTitle(Text("Welcome to JulietJamApp"))
+            .navigationBarTitleDisplayMode(.inline)
         }
         .padding()
     }
---
+++ Juliet/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Juliet
+//
+//  Created by Jaycob liu kaiwen on 12/9/26.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    DetailView()
+}
