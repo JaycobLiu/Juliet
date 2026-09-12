50cde6631268da6f7183e639e32ae8e9e89abf3d
Jaycob Liu <Football@2013_20>
2026-09-12 16:35:41 +0800

    y

--- Juliet/DetailView.swift
+++ Juliet/DetailView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct DetailView: View {
+    @State var juliet = false
     var body: some View {
-        
+        NavigationStack{
+            TabView{
+                Tab("Ingredients",systemImage:"checklist"){
+                    
+                    
+                    List{
+                        HStack{
+                            //make a context menu for each ingredient
+                            Toggle("Juliet", isOn:$juliet)
+                            
+                        }
+                    }
+                    
+                    .navigationTitle(Text("What you need for Juliet Jam"))
+                    .navigationBarTitleDisplayMode(.inline)
+                }
+                
+                .tabPlacement(.sidebarOnly)
+            }
+            
+        }
     }
 }
 
